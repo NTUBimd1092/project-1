c4e2696dfc07092c9e546f35da6565d98ee0988b
YaunYaun <10836016@ntub.edu.tw>
2020-03-13 14:34:51 +0800

    爬蟲程式
    新增資料庫資料

--- python/Accenture.py
+++ python/Accenture.py
@@ -8,5 +8,24 @@ with req.urlopen(url) as response:
     data=response.read().decode("utf-8")
 import bs4
 root=bs4.BeautifulSoup(data,"html.parser")
+pdf
 print(root.title.string)
+#連線資料庫
+import pymysql
+db = pymysql.connect("localhost","root","1234","crawler" )
+cursor = db.cursor()
+cursor.execute("SELECT VERSION()")
+data = cursor.fetchone()
+#插入資料
+sqlinsert = """INSERT INTO page_data(pdf,content)
+         VALUES ('pyhon','python content insert test')"""
+try:
+   # 執行sql語句
+   cursor.execute(sqlinsert)
+   # 提交到資料庫執行
+   db.commit()
+except:
+   # 如果發生錯誤則回滾
+   db.rollback()
+db.close()
 
--- python/sql.py
+++ python/sql.py
@@ -3,8 +3,6 @@ db = pymysql.connect("localhost","root","1234","crawler" )
 cursor = db.cursor()
 cursor.execute("SELECT VERSION()")
 data = cursor.fetchone()
-print ("Database version : %s " % data)
-
 sql = "SELECT * FROM `menber` WHERE 1"
 try:
    # 執行SQL語句
@@ -17,3 +15,16 @@ try:
       print (fname)
 except:
    print ("Error: unable to fetch data")
+
+sqlinsert = """INSERT INTO page_data(pdf,content)
+         VALUES ('pyhon','python content insert test')"""
+try:
+   # 執行sql語句
+   cursor.execute(sqlinsert)
+   # 提交到資料庫執行
+   db.commit()
+except:
+   # 如果發生錯誤則回滾
+   db.rollback()
+db.close()
+
