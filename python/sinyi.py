5d6c3a11957511d68bc6de95a15855fbdb00dd00
YaunYaun <10836016@ntub.edu.tw>
2020-04-09 20:20:31 +0800

    完成信義屋網的資料
    給聖哲的SQL範本

--- python/sinyi.py
+++ python/sinyi.py
@@ -1,12 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
-url="https://www.sinyi.com.tw/rent/"
-res= requests.get(url)
-soup=BeautifulSoup(res.text,'html.parser')
-result= set()
-for link in soup.select('.search_result_item'):
-    result.add('https://www.sinyi.com.tw/rent/'+link.select_one('a')['href'])
-result = list(result)
-for i ,result in enumerate(result):
-    print(i,'-->',result) 
-
+def getData(url):
+    res= requests.get(url)
+    soup=BeautifulSoup(res.text,'html.parser')#原始碼
+    adress=soup.find_all(class_="num num-text")#地址
+    print("地址============================")
+    for adres in adress:
+        if soup.find(class_="num num-text") !=None:
+            print(adres.string)
+    print("房屋名稱============================")
+    house=soup.find_all("span", class_="item_title")#房屋名稱
+    for name in house:
+        if soup.find("span", class_="item_title")!=None:
+            print(name.string)       
+    print("房屋網址============================")
+    result= set()#房屋網址
+    for link in soup.select('.search_result_item'):
+        result.add('https://www.sinyi.com.tw/rent/'+link.select_one('a')['href'])      
+    result = list(result)
+    for i ,result in enumerate(result):
+        print(result)
+        
+count=1
+while count<=2:
+    pageURL="https://www.sinyi.com.tw/rent/list/"+str(count)+".html"
+    print("==========================================")
+    print(pageURL)
+    pageURL=getData(pageURL)
+    count+=1
--- python/sql.py
+++ python/sql.py
@@ -3,28 +3,19 @@ db = pymysql.connect("localhost","root","1234","crawler" )
 cursor = db.cursor()
 cursor.execute("SELECT VERSION()")
 data = cursor.fetchone()
-sql = "SELECT * FROM `menber` WHERE 1"
-try:
-   # 執行SQL語句
-   cursor.execute(sql)
-   # 獲取所有記錄列表
-   results = cursor.fetchall()
-   for row in results:
-      fname = row[1]
-       # 列印結果
-      print (fname)
-except:
-   print ("Error: unable to fetch data")
-
-sqlinsert = """INSERT INTO page_data(pdf,content)
-         VALUES ('pyhon','python content insert test')"""
+#插入資料
+sqlinsert = ("INSERT INTO page_data(Link,house,adress)" "VALUES(%s,%s,%s)")
+data_inster = ('','','')
 try:
    # 執行sql語句
-   cursor.execute(sqlinsert)
+   cursor.execute(sqlinsert, data_inster)
    # 提交到資料庫執行
    db.commit()
+   print('成功')
 except:
    # 如果發生錯誤則回滾
    db.rollback()
+   print('失敗')
+
 db.close()
 
