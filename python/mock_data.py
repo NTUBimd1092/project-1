4ace6da6f6eedc9ccf500be27c2125e6c02d4fb6
YaunYaun <10836016@ntub.edu.tw>
2020-04-29 21:55:44 +0800

    樂屋網
    完成

--- python/mock_data.py
+++ python/mock_data.py
@@ -1,24 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
-url="https://www.sinyi.com.tw/rent/list/1.html"
+url="https://www.rakuya.com.tw/search/rent_search/index?display=list&con=&tab=def&sort=11&ds=&page=1"
 resources = requests.get(url)
 soup = BeautifulSoup(resources.text, 'html.parser')
+
 result= list()
-i=0
-for house in soup.select('.search_result_item'):
-    page=house.find("div",class_="detail_line2")
-    Ineed=page.find_all("span",class_="num")
-    string=[]
-    i+=1
-    for x in Ineed:
-        string.append(str(x.string))
-    print(f'#{i}:')
-    print(string[0])
-    print(string[1])
-    print(string[2]+string[3])
-    print(string[3])
-    print(string[4])
-    print(string[5])
-    print(string[6])
-    print(f'樓層:{x.string}')
-    #print(Ineed.split(","))   
+
+for house in soup.select('.obj-info'):
+    house_name=house.find('a').string
+    house_address=house.find(class_="obj-address").string
+    house_url=house.select_one('a')['href']
+    house_money=house.find('span').string[:-1]
+    house_info1=house.find_all('span')
+    house_info2=[]
+    for x in house_info1:
+        house_info2.append(str(x.string))
+    result.append({
+        'Link': house_url,
+        'house_name':house_name,
+        'address': house_address,
+        'house_money':house_money,
+        'house_type':house_info2[1],
+        'floor':house_info2[4],
+        'square_meters':house_info2[3][:-1],
+        'pattern':house_info2[2],
+    })
+
+for i, data in enumerate(result):
+    print(f'#{i}: ')
+    print(data['Link'])
+    print(data['house_name'])
+    print(data['address'])
+    print(data['house_money'])
+    print(data['house_type'])
+    print(data['floor'])
+    print(data['square_meters'])
+    print(data['pattern'])
+    print()
+print(f'Total: {len(result)}')
+print(soup.find(class_="pages").string[5:-1])
---
+++ python/rakuya.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import pymysql
+db = pymysql.connect("localhost","root","1234","crawler")
+cursor = db.cursor()
+
+def getData(url):
+    resources = requests.get(url)
+    soup = BeautifulSoup(resources.text, 'html.parser')
+
+    result= list()
+
+    for house in soup.select('.obj-info'):
+        house_name=house.find('a').string
+        house_address=house.find(class_="obj-address").string
+        house_url=house.select_one('a')['href']
+        house_money=house.find('span').string[:-1]
+        house_info1=house.find_all('span')
+        house_info2=[]
+        for x in house_info1:
+            house_info2.append(str(x.string))
+        result.append({
+            'Link': house_url,
+            'house_name':house_name,
+            'address': house_address,
+            'house_money':house_money,
+            'house_type':house_info2[1],
+            'floor':house_info2[4],
+            'square_meters':house_info2[3][:-1],
+            'pattern':house_info2[2],
+        })
+    for i, data in enumerate(result):
+        print(f'#{i}: ')
+        print(data['Link'])
+        print(data['house_name'])
+        print(data['address'])
+        print(data['house_money'])
+        print(data['house_type'])
+        print(data['floor'])
+        print(data['square_meters'])
+        print(data['pattern'])
+        print()
+  
+        sqlinsert = ("INSERT INTO page_data(WebName,Link,house,adress,money,house_type,floor,square_meters,pattern)" "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+        val = ['樂屋網',data['Link'],data['house_name'],data['address'],data['house_money'],data['house_type'],data['floor'],data['square_meters'],data['pattern']]
+        cursor.execute(sqlinsert,val)
+        db.commit()
+    print(f'Total: {len(result)}')
+    return soup.find(class_="pages").string[5:-1]
+
+
+count=1#頁數
+while count<=5:
+    pageURL="https://www.rakuya.com.tw/search/rent_search/index?display=list&con=&tab=def&sort=11&ds=&page="+str(count)+".html"
+    print(f'=================第{count}頁==================')
+    print(pageURL)
+    pageURL=getData(pageURL)
+    count+=1
+db.close()
