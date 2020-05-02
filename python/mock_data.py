824fc11f616a4fea50a84df27dd43810cde0f670
YaunYaun <10836016@ntub.edu.tw>
2020-05-02 15:07:00 +0800

    樂屋網
    加圖片

--- python/mock_data.py
+++ python/mock_data.py
@@ -6,36 +6,38 @@ soup = BeautifulSoup(resources.text, 'html.parser')
 
 result= list()
 
-for house in soup.select('.obj-info'):
-    house_name=house.find('a').string
-    house_address=house.find(class_="obj-address").string
+for house in soup.select('.obj-item'):
+    house_mask=[]
+    for a in house.select('a'):
+        house_mask.append(a)
     house_url=house.select_one('a')['href']
-    house_money=house.find('span').string[:-1]
+    house_address=house.find(class_="obj-address").string
     house_info1=house.find_all('span')
     house_info2=[]
     for x in house_info1:
         house_info2.append(str(x.string))
     result.append({
+        'image':str(house_mask[0]['style'][22:-2]),
         'Link': house_url,
-        'house_name':house_name,
+        'house_name':house_mask[1].string,
         'address': house_address,
-        'house_money':house_money,
-        'house_type':house_info2[1],
-        'floor':house_info2[4],
-        'square_meters':house_info2[3][:-1],
-        'pattern':house_info2[2],
+        'house_money':house_info2[3][:-1],
+        'house_type':house_info2[4],
+        'floor':house_info2[7],
+        'square_meters':house_info2[6][:-1],
+        'pattern':house_info2[5]
     })
 
 for i, data in enumerate(result):
     print(f'#{i}: ')
-    print(data['Link'])
-    print(data['house_name'])
-    print(data['address'])
-    print(data['house_money'])
-    print(data['house_type'])
-    print(data['floor'])
-    print(data['square_meters'])
-    print(data['pattern'])
+    print('圖片：'+data['image'])
+    print('網址：'+data['Link'])
+    print('房名：'+data['house_name'])
+    print('地址：'+data['address'])
+    print('價格：'+data['house_money'])
+    print('類型：'+data['house_type'])
+    print('樓層：'+data['floor'])
+    print('坪數：'+data['square_meters'])
+    print('房型：'+data['pattern'])
     print()
 print(f'Total: {len(result)}')
-print(soup.find(class_="pages").string[5:-1])
--- python/rakuya.py
+++ python/rakuya.py
@@ -10,47 +10,50 @@ def getData(url):
 
     result= list()
 
-    for house in soup.select('.obj-info'):
-        house_name=house.find('a').string
-        house_address=house.find(class_="obj-address").string
+    for house in soup.select('.obj-item'):
+        house_mask=[]
+        for a in house.select('a'):
+            house_mask.append(a)
         house_url=house.select_one('a')['href']
-        house_money=house.find('span').string[:-1]
+        house_address=house.find(class_="obj-address").string
         house_info1=house.find_all('span')
         house_info2=[]
         for x in house_info1:
             house_info2.append(str(x.string))
         result.append({
+            'image':str(house_mask[0]['style'][22:-2]),
             'Link': house_url,
-            'house_name':house_name,
+            'house_name':house_mask[1].string,
             'address': house_address,
-            'house_money':house_money,
-            'house_type':house_info2[1],
-            'floor':house_info2[4],
-            'square_meters':house_info2[3][:-1],
-            'pattern':house_info2[2],
+            'house_money':house_info2[3][:-1],
+            'house_type':house_info2[4],
+            'floor':house_info2[7],
+            'square_meters':house_info2[6][:-1],
+            'pattern':house_info2[5]
         })
     for i, data in enumerate(result):
         print(f'#{i}: ')
-        print(data['Link'])
-        print(data['house_name'])
-        print(data['address'])
-        print(data['house_money'])
-        print(data['house_type'])
-        print(data['floor'])
-        print(data['square_meters'])
-        print(data['pattern'])
+        print('圖片：'+data['image'])
+        print('網址：'+data['Link'])
+        print('房名：'+data['house_name'])
+        print('地址：'+data['address'])
+        print('價格：'+data['house_money'])
+        print('類型：'+data['house_type'])
+        print('樓層：'+data['floor'])
+        print('坪數：'+data['square_meters'])
+        print('房型：'+data['pattern'])
         print()
   
-        sqlinsert = ("INSERT INTO page_data(WebName,Link,house,adress,money,house_type,floor,square_meters,pattern)" "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)")
-        val = ['樂屋網',data['Link'],data['house_name'],data['address'],data['house_money'],data['house_type'],data['floor'],data['square_meters'],data['pattern']]
+        sqlinsert = ("INSERT INTO page_data(WebName,images,Link,house,adress,money,house_type,floor,square_meters,pattern)" "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+        val = ['樂屋網',data['image'],data['Link'],data['house_name'],data['address'],data['house_money'],data['house_type'],data['floor'],data['square_meters'],data['pattern']]
         cursor.execute(sqlinsert,val)
         db.commit()
     print(f'Total: {len(result)}')
-    return soup.find(class_="pages").string[5:-1]
+    # return soup.find(class_="pages").string[5:-1]總頁數
 
 
 count=1#頁數
-while count<=5:
+while count<=2:
     pageURL="https://www.rakuya.com.tw/search/rent_search/index?display=list&con=&tab=def&sort=11&ds=&page="+str(count)+".html"
     print(f'=================第{count}頁==================')
     print(pageURL)
