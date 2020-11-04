fc186a1a9db2e3739f6c6b6d693634f49f48884c
pony <10836017@ntub.edu.tw>
2020-11-04 17:07:24 +0800

    Google logo大小修正、收藏.js補傳

--- page/rent/src/get_data.js
+++ page/rent/src/get_data.js
@@ -1,22 +1,27 @@
-// JavaScript Document
-/*
-$(document).ready(function(){
-	var i=0;
-	$(".favorite").click(function(){
-		// Change src attribute of image
-		if(i==0){
-			$(this).attr("src", "images/selectedFav.png");
-			i+=1;
-		}else{
-			$(this).attr("src", "images/favorite.png");
-			i-=1;		
-		}
-	});  
-	
-});*/
-
-function Favorate(myObj,userid){
-		alert(myObj.id+userid);
-		
-	
-	}
+function Favorate(myObj, userid) {
+	$(document).ready(function () {
+		$.ajax({
+			type: "POST",
+			url: "get_data.php",
+			data: {
+				'Action': 'Favorate',
+				'Link': myObj.id,
+				'userid': userid
+			},
+			contentType: "application/x-www-form-urlencoded; charset=utf-8",
+			success: function (data) {
+				var text = data.substr(-6);
+				if (text == 'Insert') {
+					// alert("收藏");
+					$(myObj).attr("src", "images/selectedFav.png");
+				} else if (text == 'Delete') {
+					// alert("移除");
+					$(myObj).attr("src", "images/favorite.png");
+				}
+			},
+			error: function (e) {
+				console.log("error", e)
+			}
+		});
+	})
+}
