725e0a1273968cad186ddc0aab8404b100d90e0e
pony <10836017@ntub.edu.tw>
2020-11-05 19:16:05 +0800

    新增index.php、Connections密碼修改、檔案整理

--- page/rent/Connections/cralwer.php
+++ page/rent/Connections/cralwer.php
@@ -5,6 +5,6 @@
 $hostname_cralwer = "localhost";
 $database_cralwer = "crawler";
 $username_cralwer = "root";
-$password_cralwer = "1234";
+$password_cralwer = "xu.61i6u;6";
 $cralwer = mysql_pconnect($hostname_cralwer, $username_cralwer, $password_cralwer) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
